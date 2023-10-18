6f6a4fdc79c92e78b879c916404b1eb1b075f084
Siddharthpratapsingh1 <spsiddharth14@gmail.com>
2023-10-18 11:52:09 +0530

    comments is add

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 let string = " ";
 let buttons = document.querySelectorAll('.button');
-Array.from(buttons).forEach((button)=>{
+Array.from(buttons).forEach((button)=>{    //make Array of all selected buttons
     button.addEventListener('click', (e)=>{
         if(e.target.innerHTML == '='){
-            string = eval(string);
+            string = eval(string);   // to evaluate the mathematical expression stored in the string variable
             document.querySelector('input').value = string;
         }
         else if(e.target.innerHTML == 'C'){
@@ -11,8 +11,8 @@ Array.from(buttons).forEach((button)=>{
             document.querySelector('input').value = string;
         }
         else{
-        console.log(e.target)
-        string = string + e.target.innerHTML;
+        // console.log(e.target)
+        string = string + e.target.innerHTML; //Append the next click button string 
         document.querySelector('input').value = string;}
     })
 
